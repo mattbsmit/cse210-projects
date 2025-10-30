8f2b70c88ea179fd53e5407eea83b88703b062db
mattbsmit <mattbsmit@byui.edu>
2025-10-30 09:55:46 -0600

    Programming Parts 1 - 2 Completed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter your grade percentage: ");
+        string valueInText = Console.ReadLine();
+        int gradePercentage = int.Parse(valueInText);
+
+        if (gradePercentage >= 90)
+        {
+            Console.WriteLine("Your letter grade is A");
+        }
+        else if (gradePercentage >= 80)
+        {
+            Console.WriteLine("Your letter grade is B");
+        }
+        else if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Your letter grade is C");
+        }
+        else if (gradePercentage >= 60)
+        {
+            Console.WriteLine("Your letter grade is D");
+        }
+        else
+        {
+            Console.WriteLine("Your letter grade is F");
+        }
     }
 }
