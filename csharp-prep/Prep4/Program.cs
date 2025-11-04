c0b3daa70503b5d27f5da6d7b52d771282d9e5bd
mattbsmit <mattbsmit@byui.edu>
2025-11-04 10:38:07 -0700

    Prep 4 - Finished

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> list = new List<int>();
+        int userNumber;
+        int sum = 0;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished");
+
+        do
+        {
+            Console.Write("Enter a number: ");
+            string enteredNumber = Console.ReadLine();
+            userNumber = int.Parse(enteredNumber);
+            
+            list.Add(userNumber);
+        } while (userNumber != 0);
+
+        foreach (int item in list)
+        {
+            sum = sum + item;
+        }
+
+        int listSize = list.Count;
+        int average = sum / listSize;
+        int largeNumber = list.Max(); 
+
+    
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largeNumber}");
     }
 }
