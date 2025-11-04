e2844c5317d21c9d3ddb8857b623550492ded1d2
mattbsmit <mattbsmit@byui.edu>
2025-11-04 09:04:51 -0700

    Prep 3 - Finished

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomNumber = new Random();
+        int magicNumber = randomNumber.Next(1, 101);
+        int userGuess;
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            string guessNumber = Console.ReadLine();
+            userGuess = int.Parse(guessNumber);
+
+            if (userGuess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (userGuess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        } while (userGuess != magicNumber);
     }
 }
